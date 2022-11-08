444bca011ae8d2337c8042cade836ab72cbf182e
User <tazo.berdzenidze@gmail.com>
2022-11-08 22:32:08 +0400

    task

--- tamazi.js
+++ tamazi.js
@@ -1,1 +1,16 @@
-console.log("im tamazi");
+function Submit() {
+    if (document.forms["Registrationform"]["firstname"].value == "") {
+      alert("Details must be filled out");
+      return false;
+    }
+    else if (document.forms["Registrationform"]["lastname"].value == "") {
+      alert("Details must be filled out");
+      return false;
+    }
+    else if (document.forms["Registrationform"]["address"].value == "") {
+      alert("Tamazi wants your name and address");
+      return false;
+    }
+  
+  } 
+  
